b63dfc1a61a51ab85023dd25ede3da93d030d392
teresa <teresasilver13@gmail.com>
2023-10-09 00:05:30 +0530

    changed the color and added padding

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,36 +1,68 @@
-
 import 'package:flutter/material.dart';
-
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-   @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-  appBar: AppBar(
-        title: const Text('Login Page'),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+         gradient: LinearGradient(
+          colors:[
+            Color.fromARGB(255, 53, 37, 228),
+            Colors.pink,
+            ]),
+        ),
+      child: const Padding(
+          padding: EdgeInsets.all(100.0),
+          child: Text('Name',style:TextStyle(
+            fontSize:40,
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+          ),),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:250.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40)
                 ),
+                color:Colors.white,
               ),
-              const SizedBox(height: 16.0),
-              const TextField(
-                obscureText: true, // Use this for password input
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              ElevatedButton(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment:MainAxisAlignment.center ,
+                children:[
+                  const Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'User Name',  
+                      hintText: 'Enter Your Name',  
+                    ),  
+                  ),  
+                ),  
+                const Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    obscureText: true,  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Password',  
+                      hintText: 'Enter Password',  
+                    ),  
+                  ),  
+                ),  
+ElevatedButton(
                 onPressed: () {
                   // Add your login logic here
                   // Typically, you'd validate the credentials and navigate to the next screen on success.
@@ -39,16 +71,12 @@ class LoginPage extends StatelessWidget {
                 },
                 child: const Text('Login'),
               ),
-            ],
-          ),
-        ),
-  
-      ) 
+
+                ]),
+               ),
+               ),
+        ],
+      )
     );
   }
 }
-
-
-
-   
-
