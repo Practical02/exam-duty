d87480bff33df8d345a430d94f7104334d4d09e7
teresa <teresasilver13@gmail.com>
2023-10-08 23:26:43 +0530

    undid all the changes

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,56 +1,54 @@
+
 import 'package:flutter/material.dart';
+
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-         gradient: LinearGradient(
-          colors:[
-            Color.fromARGB(255, 53, 37, 228),
-            Colors.pink,
-            ]),
-        ),
-      child: const Padding(
-          padding: EdgeInsets.all(100.0),
-          child: Text('Name',style:TextStyle(
-            fontSize:40,
-            color: Colors.white, 
-            fontWeight: FontWeight.bold
-          ),),
+  appBar: AppBar(
+        title: const Text('Login Page'),
       ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top:250.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40)
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
                 ),
-                color:Colors.white,
               ),
-              height: double.infinity,
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment:MainAxisAlignment.center ,
-                children:[
-                  
-                ]),
-               ),
-               ),
-        ],
+              const SizedBox(height: 16.0),
+              const TextField(
+                obscureText: true, // Use this for password input
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your login logic here
+                  // Typically, you'd validate the credentials and navigate to the next screen on success.
+                  // For this example, we'll just navigate to the user dashboard.
+                  Navigator.pushReplacementNamed(context, '/user');
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+  
       ) 
     );
-        
   }
-}     
+}
+
+
 
    
+
