661d41e8af2bd4bc8de8fe93d4a704c8e1ae548c
liyafrancis <francisliya03@gmail.com>
2023-10-08 20:50:24 +0530

    card created using stateless widget

--- lib/screens/user_dashboard.dart
+++ lib/screens/user_dashboard.dart
@@ -5,22 +5,47 @@ class UserDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Dashboard'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to the User Dashboard!',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('User Dashboard'),
+          ),
+          body: Align(
+            alignment: Alignment.topLeft,
+            child: Mycard(),
+          )),
     );
+  }
+}
+
+class Mycard extends StatelessWidget {
+  const Mycard({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 300,
+        height: 150,
+        padding: new EdgeInsets.all(10.0),
+        child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                side: BorderSide(
+                  color: Color.fromARGB(255, 143, 40, 129),
+                )),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  title: Text(
+                    'DUTY ON : ',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  subtitle:
+                      Text(
+                        '12-04-2023', style: TextStyle(fontSize: 18.0)
+                        ),
+                )
+              ],
+            )));
   }
 }
