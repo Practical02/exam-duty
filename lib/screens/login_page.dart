0d45d512f10d364941b4554cb2c44d31519c300a
Amal Manoj <amalmanoj02@gmail.com>
2023-10-14 21:15:36 +0530

    some minor tweakings ui

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,94 +5,105 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Colors.blueAccent,
-              Color.fromARGB(255, 166, 8, 198),
-            ]),
-          ),
-          child: const Padding(
-            padding: EdgeInsets.all(100.0),
-            child: Text(
-              'Name',
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 5.0, top: 230.0, right: 5.0),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(80),
-                topRight: Radius.circular(80),
-              ),
-              color: Color.fromARGB(87, 255, 255, 255),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 250.0),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(50), topRight: Radius.circular(50)),
-              color: Colors.white,
-            ),
+      body: Stack(
+        children: [
+          Container(
             height: double.infinity,
             width: double.infinity,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(150, 20, 150, 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(155, 233, 227, 227)),
-                        borderRadius: BorderRadius.circular(10.0)),
-                    labelText: 'Email Address',
-                    hintText: 'Enter your email',
-                  ),
-                ),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blueAccent,
+                  Color.fromARGB(255, 166, 8, 198),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(150, 20, 150, 20),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color.fromARGB(155, 233, 227, 227)),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                  ),
-                ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(100.0),
+              child: Text(
+                'Name',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your login logic here
-                  // Typically, you'd validate the credentials and navigate to the next screen on success.
-                  // For this example, we'll just navigate to the user dashboard.
-                  Navigator.pushReplacementNamed(context, '/user');
-                },
-                child: const Text('Login'),
-              )
-            ]),
+            ),
           ),
-        ),
-      ],
-    ));
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0, top: 230.0, right: 5.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(80),
+                  topRight: Radius.circular(80),
+                ),
+                color: Color.fromARGB(87, 255, 255, 255),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 250.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                color: Colors.white,
+              ),
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(155, 233, 227, 227)),
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: 'Email Address',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 20, 50, 50),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color.fromARGB(155, 233, 227, 227),
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/user');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
